92e6aedcbf7b122bf5a4561cabbe726ed662c263
Mohamed Ramadan Qamar <mor@crossworkers.com>
2025-03-10 07:57:39 +0200

    shop pro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { MyOrderComponent } from './my-order/my-order.component';
+import { AdminOrdersComponent } from './Amdin/admin-orders/admin-orders.component';
+import { AdminProductsComponent } from './Amdin/admin-products/admin-products.component';
+import { ShopingCartComponent } from './shoping-cart/shoping-cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'products',component:ProductsComponent},
+  {path:'orders',component:MyOrderComponent},
+  {path:'admin/orders',component:AdminOrdersComponent},
+  {path:'admin/products',component:AdminProductsComponent},
+  {path:'Shopingcart',component:ShopingCartComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ProductsComponent } from './products/products.component';
+import { MyOrderComponent } from './my-order/my-order.component';
+import { AdminOrdersComponent } from './Amdin/admin-orders/admin-orders.component';
+import { AdminProductsComponent } from './Amdin/admin-products/admin-products.component';
+import { ShopingCartComponent } from './shoping-cart/shoping-cart.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ProductsComponent,
+    MyOrderComponent,
+    AdminOrdersComponent,
+    AdminProductsComponent,
+    ShopingCartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
